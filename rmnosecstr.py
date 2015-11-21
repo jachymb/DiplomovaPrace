28743a55121cd695d3d2afa00b567a9c17b873fc
Jáchym Barvínek <jachymb@gmail.com>
2015-11-21 12:52:04 +0100

    rmnosecstr

--- rmnosecstr.py
+++ rmnosecstr.py
@@ -4,7 +4,7 @@
 from dp.utils import parseFasta
 
 dataset = "data/dataset.txt"
-g = dict(parseFasta("data/ss.txt"))
+ss = dict(parseFasta("data/ss.txt"))
 with open(dataset, "r") as f:
     data = f.read().splitlines()
 
@@ -14,7 +14,7 @@ with open(dataset, "w") as f:
     for g in data:
         name, strand = g.split("_")
         name = name.upper()
-        if ("%s:%s:secstr" % (name, strand)) in g:
+        if ("%s:%s:secstr" % (name, strand)) in ss:
             r.append(g)
     f.write("\n".join(r))
 
